5ead7be97c2cc8a7ec6e694451151f70f7d85a1f
zhoubin <zhoubin1@gemdale.com>
2016-06-08 09:21:32 +0800

    fixed TypeError: Cannot read property '0' of undefined

--- index.js
+++ index.js
@@ -9,46 +9,46 @@ var PLUGIN_NAME = 'gulp-amd-idfy';
 console.log(PLUGIN_NAME + ' started');
 
 module.exports = function() {
-    
-  function findDefine(body) {
-    for (var i = 0; i < body.length; i++) {
-      if (body[i].type === 'ExpressionStatement'
-         && body[i].expression._paths[0].node.type === 'Identifier'
-         && body[i].expression._paths[0].node.name === 'define') {
-        return body[i];
-      }      
-    }
-    return null;
-  }
-  
-  return through.obj(function(file, encoding, callback) {
-      if (file.isNull()) {
-        // nothing to do
-        return callback(null, file);
-      }
 
-      if (file.isStream()) {
-        // file.contents is a Stream - https://nodejs.org/api/stream.html
-        this.emit('error', new PluginError(PLUGIN_NAME, 'Streams not supported!'));
-      } else if (file.isBuffer()) {
-        // file.contents is a Buffer - https://nodejs.org/api/buffer.html
-        var result = babel.transform(file.contents.toString());
-        
-        // Search for the define() call at the root of the AST.
-        var defineNode = findDefine(result.ast.program.body);
-        if (defineNode && defineNode.expression.arguments.length) {
-          // If the first argument of define is a string literal that means the module already 
-          // has a name.
-          if (defineNode.expression.arguments[0].type !== 'StringLiteral') {
-            defineNode.expression.arguments.splice(0, 0, {
-              "type": "StringLiteral",
-              "value": path.relative(file.base, file.path).match(/(.*)\.[^.]+$/, '')[1]
-            });
-            file.contents = new Buffer(babel.transformFromAst(result.ast).code);
-          }
+    function findDefine(body) {
+        for (var i = 0; i < body.length; i++) {
+            if (body[i].type === 'ExpressionStatement'
+                && body[i].expression.callee.type === 'Identifier'
+                && body[i].expression.callee.name === 'define') {
+                return body[i];
+            }
         }
-        
-        return callback(null, file);
-      }
-  });
+        return null;
+    }
+
+    return through.obj(function(file, encoding, callback) {
+        if (file.isNull()) {
+            // nothing to do
+            return callback(null, file);
+        }
+
+        if (file.isStream()) {
+            // file.contents is a Stream - https://nodejs.org/api/stream.html
+            this.emit('error', new PluginError(PLUGIN_NAME, 'Streams not supported!'));
+        } else if (file.isBuffer()) {
+            // file.contents is a Buffer - https://nodejs.org/api/buffer.html
+            var result = babel.transform(file.contents.toString());
+
+            // Search for the define() call at the root of the AST.
+            var defineNode = findDefine(result.ast.program.body);
+            if (defineNode && defineNode.expression.arguments.length) {
+                // If the first argument of define is a string literal that means the module already
+                // has a name.
+                if (defineNode.expression.arguments[0].type !== 'StringLiteral') {
+                    defineNode.expression.arguments.splice(0, 0, {
+                        "type": "StringLiteral",
+                        "value": path.relative(file.base, file.path).replace(path.sep,'/').match(/(.*)\.[^.]+$/, '')[1]
+                    });
+                    file.contents = new Buffer(babel.transformFromAst(result.ast).code);
+                }
+            }
+
+            return callback(null, file);
+        }
+    });
 };
